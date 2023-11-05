dc4335391c593804c432b3fd87ed8b6e8eb48e1c
Almuhannad <almuhannad.khalid@gmail.com>
2023-11-05 17:50:24 +0300

    retrying

---
+++ src/main/java/com/luminousstore/luminousstore/LuminousStoreApplication.java
@@ -0,0 +1,13 @@
+package com.luminousstore.luminousstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminousStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuminousStoreApplication.class, args);
+    }
+
+}
