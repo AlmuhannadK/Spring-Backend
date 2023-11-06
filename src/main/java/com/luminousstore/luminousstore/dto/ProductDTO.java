9567692d45aaa288c1727e4804cf91d857b14526
Almuhannad <almuhannad.khalid@gmail.com>
2023-11-06 17:01:39 +0300

    dtos for later use.

--- src/main/java/com/luminousstore/luminousstore/dto/ProductDTO.java
+++ src/main/java/com/luminousstore/luminousstore/dto/ProductDTO.java
@@ -1,4 +1,26 @@
 package com.luminousstore.luminousstore.dto;
 
+import com.luminousstore.luminousstore.entity.ProductCategory;
+
+import java.math.BigDecimal;
+
 public class ProductDTO {
+
+    private String sku;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal unitPrice;
+
+    private String imageUrl;
+
+    private int unitsInStock;
+
+    private ProductCategory category;
+
+
 }
+
+
