c8b4da195ffd69279fb83d4c83ca51103779da3e
Almuhannad <almuhannad.khalid@gmail.com>
2023-11-06 17:05:41 +0300

    forgot the orderDto too

---
+++ src/main/java/com/luminousstore/luminousstore/dto/OrderDTO.java
@@ -0,0 +1,15 @@
+package com.luminousstore.luminousstore.dto;
+
+import java.math.BigDecimal;
+
+public class OrderDTO {
+
+    private String orderTrackingNumber;
+
+    private int totalQuantity;
+
+    private BigDecimal totalPrice;
+
+    private String status;
+
+}
