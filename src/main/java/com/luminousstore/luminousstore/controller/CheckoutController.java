cd9e67556e0ce094f286bff9486a266b85cecf07
Almuhannad <almuhannad.khalid@gmail.com>
2023-11-07 14:45:50 +0300

    done with checkout

--- src/main/java/com/luminousstore/luminousstore/controller/CheckoutController.java
+++ src/main/java/com/luminousstore/luminousstore/controller/CheckoutController.java
@@ -1,6 +1,12 @@
 package com.luminousstore.luminousstore.controller;
 
+import com.luminousstore.luminousstore.dto.PurchaseRequest;
+import com.luminousstore.luminousstore.dto.PurchaseResponse;
+import com.luminousstore.luminousstore.service.Impl.CheckoutServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CheckoutController {
 
+    private final CheckoutServiceImpl checkoutService;
+
+
+    @PostMapping(path = "/purchase")
+    public ResponseEntity<PurchaseResponse> placeOrder(@RequestBody PurchaseRequest purchase) {
+        return ResponseEntity.ok(this.checkoutService.placeOrder(purchase));
+    }
 
 }
